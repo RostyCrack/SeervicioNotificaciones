6c81200a700d8780f96a85fa377a1e2dcc03dae3
Cristian David Rosero Suárez <78422799+RostyCrack@users.noreply.github.com>
2024-03-08 14:40:44 -0500

    initial commit

--- src/main/java/com/botech/servicionotificaciones/Resquest.java
+++ src/main/java/com/botech/servicionotificaciones/Resquest.java
@@ -1,18 +1,30 @@
 package com.botech.servicionotificaciones;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
 public class Resquest {
 
-    public void sendRequest() {
+    public void sendRequest() throws FirebaseMessagingException {
         log.info("Sending request...");
-        String response = WebClient.create()
-                .get()
-                .uri("http://localhost:8080")
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        // This registration token comes from the client FCM SDKs.
+        String registrationToken = "YOUR_REGISTRATION_TOKEN";
+
+// See documentation on defining a message payload.
+        Message message = Message.builder()
+                .putData("score", "850")
+                .putData("time", "2:45")
+                .setToken(registrationToken)
+                .build();
+
+// Send a message to the device corresponding to the provided
+// registration token.
+        String response = FirebaseMessaging.getInstance().send(message);
+// Response is a message ID string.
+        System.out.println("Successfully sent message: " + response);
     }
 }
