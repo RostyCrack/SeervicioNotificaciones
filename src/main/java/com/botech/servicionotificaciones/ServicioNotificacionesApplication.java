52299f284c0fcb425ddf90062cb395d22128e985
Cristian David Rosero Suárez <78422799+RostyCrack@users.noreply.github.com>
2024-03-08 14:24:15 -0500

    initial commit

---
+++ src/main/java/com/botech/servicionotificaciones/ServicioNotificacionesApplication.java
@@ -0,0 +1,13 @@
+package com.botech.servicionotificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioNotificacionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioNotificacionesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/botech/servicionotificaciones/ServicioNotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botech.servicionotificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioNotificacionesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
